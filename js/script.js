d77e1c466205fc645965f9dab8e2c88547a2d182
Reaperking <72340160+What1sLov3RK@users.noreply.github.com>
2022-06-29 01:36:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//menu
+var menu = document.getElementById('menu');
+var mb = document.getElementById('mb');
+var close = document.getElementById('close');
+
+menu.onclick = function(){
+    mb.style.width = '100%';
+}
+
+
+close.onclick = function(){
+    mb.style.width = '0px'
+}
+        
+    
